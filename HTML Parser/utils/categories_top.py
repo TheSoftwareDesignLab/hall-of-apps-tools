fc4999be0abdb98a395b760ff00dd658000960ff
Laura Natalia Bello Jiménez <ln.bello10@uniandes.edu.co>
2020-01-28 20:33:19 -0500

    Added script to set category_top

--- HTML Parser/utils/categories_top.py
+++ HTML Parser/utils/categories_top.py
@@ -12,11 +12,18 @@ cursor = coll.find(query)
 counter = 0
 try:
     for doc in cursor:
-        if doc["category"]:
+        if doc["category"] and doc["top"]:
             id_mongo = doc["_id"]
-            new_category = doc["category"].replace("_topFree", "").replace("_topSelling", "")
+            category = doc["category"]
+            top = doc["top"]
+
+            if "editorchoice" in top.lower():
+                new_category = top
+            else:
+                new_category = category + "_" + top
+
             print(new_category)
-            newvalues = { "$set": { "category": new_category } }
+            newvalues = { "$set": { "category_top": new_category } }
             coll.update_one({"_id": id_mongo}, newvalues)
             counter = counter + 1
             print(counter)
