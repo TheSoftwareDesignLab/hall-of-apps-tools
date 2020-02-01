1e3d646f1d9a70e1d150cc6c2b4be152ae0c0b5c
aiMOjica10 <ai.mojica10@uniandes.edu.co>
2020-02-01 13:19:54 -0500

    changed script fornew reviews and dynamic path

--- HTML Parser/source.py
+++ HTML Parser/source.py
@@ -1,26 +1,21 @@
-import os
 from bs4 import BeautifulSoup
+import os
 import pymongo
 import utils.app_info as ai
 import utils.reviews_info as ri
 import utils.extra_apps_info as eai
+import sys
 
 ignore_folders = ["2017115-20171111%br%editorChoice%cc.pacer.androidapp.html",
                   "appsRetrieved.json", "processed", "raw_html", "RESULTS", "testData", "toProcess",
-                  "20180225-20180303",
-                  "20180304-20180310",
-                  "20180311-20180317",
-                  "20180318-20180324",
-                  "20180325-20180331",
-                  "20180401-20180407",
-                  "20180408-20180414",
-                  "20180415-20180421",
-                  "20180422-20180428",
-                  "20180429-20180505",
-                  "20180506-20180512",
-                  "20180513-20180519",
-                  "20180520-20180526",
-                  "20180527-20180602"]
+                  "20171105-20171111", "20171112-20171118", "20171119-20171125","20171126-20171202","20171126-20171202",
+                  "20171210-20171216","20171217-20171223","20171224-20171230","20171231-201816","20180107-20180113",
+                  "20180114-20180120","20180121-20180127","20180128-20180203","20180204-20180210","20180211-20180217",
+                  "20180218-20180224",
+                  #"20180225-20180303", "20180304-20180310","20180311-20180317","20180318-20180324","20180325-20180331",
+                  #"20180401-20180407","20180408-20180414","20180415-20180421","20180422-20180428","20180429-20180505",
+                  #"20180506-20180512","20180513-20180519","20180520-20180526","20180527-20180602"
+                  ]
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["mining"]
@@ -28,12 +23,12 @@ db = client["mining"]
 is_testing_mode = True
 
 
-def main():
-    folders = os.listdir("/Volumes/Elements/data")
+def main(path_o:str):
+    folders = os.listdir(path_o)
 
     for current_folder in folders:
         if current_folder not in ignore_folders:
-            path = f'/Volumes/Elements/data/{current_folder}'
+            path = f'{path_o}/{current_folder}'
 
             files_folder = os.listdir(path)
             files_folder.sort()
@@ -43,7 +38,7 @@ def main():
             for counter, file in enumerate(files_folder):
                 print(str(counter + 1), "-", file)
 
-                with open(os.path.join(path, file), "r") as html_file:
+                with open(os.path.join(path, file), "r", encoding="utf8") as html_file:
                     html_content = html_file.read()
 
                 dictionary = {}
@@ -55,11 +50,11 @@ def main():
                 if page_format is not None:
                     is_new_page = True
 
-                #dictionary = ai.get_basic_info(soup, file, dictionary, is_new_page)
-                #dictionary = ai.get_tech_info(soup, dictionary, is_new_page)
-                #dictionary = ai.get_rating(soup, dictionary, is_new_page)
-                #dictionary = ai.get_whats_new(soup, dictionary, is_new_page)
-                #dictionary = ai.get_dev_info(soup, dictionary, is_new_page)
+                dictionary = ai.get_basic_info(soup, file, dictionary, is_new_page)
+                dictionary = ai.get_tech_info(soup, dictionary, is_new_page)
+                dictionary = ai.get_rating(soup, dictionary, is_new_page)
+                dictionary = ai.get_whats_new(soup, dictionary, is_new_page)
+                dictionary = ai.get_dev_info(soup, dictionary, is_new_page)
 
                 dictionary, data_reviews = ri.get_reviews(soup, dictionary, is_new_page, file)
 
@@ -74,7 +69,7 @@ def main():
                 #extra_apps.extend(data_more)
 
                 #save_mongo("app", [dictionary])
-                save_mongo("review", data_reviews)
+                save_mongo("reviews2", data_reviews)
                 #save_mongo("extra_app", extra_apps)
 
                 print(f'WRITTEN {counter + 1} apps more')
@@ -90,4 +85,9 @@ def save_mongo(collection_db, data):
 
 
 if __name__ == "__main__":
-    main()
+
+    if len(sys.argv) != 2:
+        print("Root folder's path is not given")
+        sys.exit(1)
+    else:
+        main(sys.argv[1])
--- HTML Parser/utils/reviews_info.py
+++ HTML Parser/utils/reviews_info.py
@@ -1,15 +1,128 @@
-from decimal import *
 import datetime
+from decimal import *
+import json
+import re
+
+
+def get_date(string_date):
+
+    date_time_obj = datetime.datetime.strptime(string_date, "%B %d, %Y")
+    return date_time_obj
+
+
+def get_retrieved_date(retrieved_date):
+
+    return datetime.datetime.strptime(retrieved_date, "%Y%m%d")
+
+
+def script_in_text(id_tag=False,key='ds:13'):
+
+    def regex_f(tag):
+
+        return tag.name == 'script' and key in tag.get_text() and 'return' in tag.get_text() 
+
+    def regex_id(tag):
+
+        return tag.name == 'script' and '1.36880256E8' in tag.get_text() 
+
+    function = regex_id if id_tag else regex_f
+    
+    return function
+
+def get_field(field, review):
+
+    try:
+        if field=='author':
+            response=review[1][0] 
+
+        elif field=='rating':
+            response=review[2] 
+
+        elif field=='date':
+            response=review[5][0] 
+
+        elif field=='review_text':
+            response=review[4] 
+
+        elif field=='dev_name':
+            response=review[7][0] 
+
+        elif field=='dev_reply':
+            response=review[7][1] 
+
+        elif field=='dateReply':
+            response=review[7][2][0] 
+    except:
+        response = None
+    return response
+
+def get_key_reviews(soup):
+
+    script = soup.find_all(script_in_text(True))
+    txt_id_reviews = script[0].get_text()
+    key = None
+
+    try:
+        line_key = re.search('.*1.36880256E8.*\n', txt_id_reviews).group(0)
+
+        if 'ds:13' in line_key:
+            key=  'ds:13'
+        elif 'ds:14' in line_key:
+            key = 'ds:14'
+
+    except:
+        pass
+
+    return key
+
+
+def get_new_reviews(soup, dictionary):
+
+    key = get_key_reviews(soup)
+    # info reviews
+    script = soup.find_all(script_in_text(False,key)) #-------------------------------------------------missing key
+    text_script = script[0].get_text() # text for script tag
+    info_reviews = text_script[text_script.find("return")+7:text_script.find("}});")] # finds return and locates array reviews
+    info_reviews = json.loads(info_reviews)
+
+    data_reviews = []
+
+    for review in info_reviews[0]:
+
+        current_review = {
+            "rating": get_field('rating',review),
+            "author": get_field('author',review),
+            "text": get_field('review_text',review),
+            "date": get_field('date',review),
+            #developer name that answers
+            "dev_name": get_field('dev_name',review),
+            "dev_reply": get_field('dev_reply',review),
+            "dev_reply_date": get_field('dateReply',review),
+            #data from app
+            "app_id": dictionary["id"],
+            "app_retrieved_date_start": dictionary["retrieved_date_start"],
+            "app_retrieved_date_end": dictionary["retrieved_date_end"],
+            "app_name": dictionary["name"],
+            "category": dictionary["category"],
+            "country": dictionary["country"]
+        }
+
+        data_reviews.append(current_review)
+
+
+    return data_reviews
+
 
 
 def get_reviews(soup, dictionary, is_new_page, file):
+
     reviews = soup.find_all("div", {"class": "single-review"})
     amount_reviews = len(reviews) if reviews else 0
 
     if is_new_page and amount_reviews == 0:
-        amount_reviews = None
-
-    # "2017115-20171111%br%editorChoice%cc.pacer.androidapp.html"
+        amount_reviews = 0
+    
+    # basic information from the file name
     file_info = file.split("%")
     retrieved_date = file_info[0].split("-")
     retrieved_date_start_file = retrieved_date[0]
@@ -30,6 +143,7 @@ def get_reviews(soup, dictionary, is_new_page, file):
     dictionary["amount_reviews"] = amount_reviews
     data_reviews = []
 
+    # data from reviews
     for review in reviews:
 
         review_info = review.find("div", {"class": "review-header"}).find("div", {"class": "review-info"})
@@ -81,15 +195,9 @@ def get_reviews(soup, dictionary, is_new_page, file):
 
         data_reviews.append(current_review)
 
+    if is_new_page:
+        new_reviews = get_new_reviews(soup, dictionary)
+        dictionary["amount_reviews"] = dictionary["amount_reviews"]  + len(new_reviews)
+        data_reviews.extend(new_reviews)
+
     return dictionary, data_reviews
-
-
-def get_date(string_date):
-    date_time_obj = datetime.datetime.strptime(string_date, "%B %d, %Y")
-
-    return date_time_obj
-
-
-def get_retrieved_date(retrieved_date):
-
-    return datetime.datetime.strptime(retrieved_date, "%Y%m%d")
